9d73eb72466c818ae8a29ffd21f0fa2e9fe545b8
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2023-01-15 17:58:19 +0100

    add test for checkbox

---
+++ src/components/checkbox/__test__/Checkbox.test.tsx
@@ -0,0 +1,34 @@
+import Checkbox from '../Checkbox';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+let onChange = jest.fn();
+
+beforeEach(() => {
+  onChange.mockReset();
+});
+
+describe('<Checkbox />', () => {
+  it('should render without errors', () => {
+    render(<Checkbox isChecked={false} onChange={onChange} id='checkbox' />);
+  });
+  it('should have correct id and linked it with label', () => {
+    const { getByLabelText } = render(
+      <Checkbox isChecked={false} onChange={onChange} id='checkbox'>
+        Checkbox
+      </Checkbox>
+    );
+    getByLabelText(/checkbox/i);
+  });
+  it('should call onChange', async () => {
+    const user = userEvent.setup();
+
+    const { getByLabelText } = render(
+      <Checkbox isChecked={false} onChange={onChange} id='checkbox'>
+        Checkbox
+      </Checkbox>
+    );
+    await user.click(getByLabelText(/checkbox/i));
+    expect(onChange).toBeCalledTimes(1);
+  });
+});
