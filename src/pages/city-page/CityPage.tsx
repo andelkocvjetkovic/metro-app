f7e93efe31a1dabdb808dbf9806a91aca8f41d19
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2023-01-15 13:24:11 +0100

    add init work on city-page

--- src/pages/city-page/CityPage.tsx
+++ src/pages/city-page/CityPage.tsx
@@ -1,27 +1,46 @@
 import { useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import allCities from '@app/cities';
 import Select from '@app/components/select/Select';
 import HourVariable from '@app/pages/city-page/_partial/hour-variables/HourVariables';
 import DayVariables from '@app/pages/city-page/_partial/day-variables/DayVariables';
+import { API_BASE } from '@app/constants';
+import { useSettings } from '@app/hook/use-settings/useSettings';
 
 type VariableView = 'hour' | 'day' | undefined;
 
 function CityPage() {
   const params = useParams();
   const [variableView, setVariableView] = useState<VariableView>(undefined);
-  const [hourVariables, setHourVariables] = useState<Set<string>>(new Set());
-  const [dayVariables, setDayVariables] = useState<string[]>([]);
+  const [searchParam] = useSearchParams();
+
+  const hourVariables = searchParam.getAll('hourly');
+  const dayVariables = searchParam.getAll('daily');
+  const { settings } = useSettings();
 
   const city = useMemo(() => (params.cityId ? allCities.find(c => c.cityId === params.cityId) : undefined), [params]);
 
-  if (!city) return <div>No city found</div>;
+  const { isLoading, error, data } = useQuery(
+    ['city', city, hourVariables, dayVariables, settings],
+    async () => {
+      const hour = hourVariables.length > 0 ? '&hourly='.concat(hourVariables.toString()) : '';
+      const day = dayVariables.length > 0 ? '&daily='.concat(dayVariables.toString()) : '';
+      const temperatureUnit = `&temperature_unit=${settings.temperatureUnit}`;
+      const timeZone = `&timezone=${settings.timezoneUnit}`;
+      const pastDayUnit = `&past_days=${settings.pastDayUnit}`;
+      const lat = `latitude=${city?.lat}`;
+      const lng = `&longitude=${city?.lng}`;
+      return fetch(`${API_BASE}${lat}${lng}${hour}${day}${temperatureUnit}${timeZone}${pastDayUnit}`).then(res => res.json());
+    },
+    { enabled: !!city }
+  );
 
-  console.log(hourVariables);
+  if (!city) return <div>Something went wrong, try again</div>;
 
   return (
     <div className='flex flex-col'>
-      <h1 className='text-3xl text-gray-500 text-center mt-6'>Meteorologic data for {city.name}</h1>
+      <h1 className='text-3xl text-gray-500 text-center mt-6'>Meteorologic data for {city?.name}</h1>
       <div className='w-96 mx-auto mt-6'>
         <Select value={variableView} onChange={e => setVariableView(e.target.value as VariableView)}>
           <option value='' disabled>
@@ -32,19 +51,11 @@ function CityPage() {
         </Select>
       </div>
       <div className='mt-8 mx-auto'>
-        {variableView ? (
-          variableView === 'hour' ? (
-            <HourVariable
-              variables={hourVariables}
-              setVariables={(variables: Set<string>) => {
-                setHourVariables(variables);
-              }}
-            />
-          ) : (
-            <DayVariables variables={dayVariables} />
-          )
-        ) : undefined}
+        {variableView ? variableView === 'hour' ? <HourVariable /> : <DayVariables /> : undefined}
       </div>
+      {isLoading && <div>Loading ...</div>}
+      {error && <div>Something went wrong, try again</div>}
+      {data && <div>Sucess</div>}
     </div>
   );
 }
--- src/pages/city-page/_partial/hour-variables/HourVariables.tsx
+++ src/pages/city-page/_partial/hour-variables/HourVariables.tsx
@@ -1,44 +1,42 @@
 import Checkbox from '@app/components/checkbox/Checkbox';
+import { useSearchParams } from 'react-router-dom';
 
 const TEMPERATURE_2M = 'temperature_2m';
 const RELATIVEHUMIDITY_2M = 'relativehumidity_2m';
 const DEWPOINT_2M = 'dewpoint_2m';
 
-type HourVariableProps = {
-  variables: Set<string>;
-  setVariables: (variables: Set<string>) => void;
-};
-
-function HourVariable({ variables, setVariables }: HourVariableProps) {
+function HourVariable() {
+  const [searchParam, setSearchParam] = useSearchParams();
+  const variables = searchParam.getAll('hourly');
   return (
     <div className='grid grid-cols-12 gap-8'>
       <div className='col-span-3'>
         <Checkbox
           id={TEMPERATURE_2M}
-          isChecked={variables.has(TEMPERATURE_2M)}
+          isChecked={variables.includes(TEMPERATURE_2M)}
           onChange={e => {
-            if (e.target.checked) setVariables(new Set([...variables, TEMPERATURE_2M]));
-            else setVariables(new Set([...variables].filter(v => v !== TEMPERATURE_2M)));
+            if (e.target.checked) setSearchParam({ hourly: [...variables, TEMPERATURE_2M] });
+            else setSearchParam({ hourly: variables.filter(v => v !== TEMPERATURE_2M) });
           }}
         >
           Temperature (2 m)
         </Checkbox>
         <Checkbox
           id={RELATIVEHUMIDITY_2M}
-          isChecked={variables.has(RELATIVEHUMIDITY_2M)}
+          isChecked={variables.includes(RELATIVEHUMIDITY_2M)}
           onChange={e => {
-            if (e.target.checked) setVariables(new Set([...variables, RELATIVEHUMIDITY_2M]));
-            else setVariables(new Set([...variables].filter(v => v !== RELATIVEHUMIDITY_2M)));
+            if (e.target.checked) setSearchParam({ hourly: [...variables, RELATIVEHUMIDITY_2M] });
+            else setSearchParam({ hourly: variables.filter(v => v !== RELATIVEHUMIDITY_2M) });
           }}
         >
           Relative Humidity (2 m)
         </Checkbox>
         <Checkbox
           id={DEWPOINT_2M}
-          isChecked={variables.has(DEWPOINT_2M)}
+          isChecked={variables.includes(DEWPOINT_2M)}
           onChange={e => {
-            if (e.target.checked) setVariables(new Set([...variables, DEWPOINT_2M]));
-            else setVariables(new Set([...variables].filter(v => v !== DEWPOINT_2M)));
+            if (e.target.checked) setSearchParam({ hourly: [...variables, DEWPOINT_2M] });
+            else setSearchParam({ hourly: variables.filter(v => v !== DEWPOINT_2M) });
           }}
         >
           Dewpoint (2 m)
